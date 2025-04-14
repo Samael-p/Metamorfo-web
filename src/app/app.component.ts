8daf5e33f0c0a3dadc0050a09ba18e6707db8ffb
Samael-p <samm22mc@gmail.com>
2025-04-13 22:03:32 -0400

    building nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Importaciones de tus componentes standalone
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FootersComponent } from './components/footers/footers.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FootersComponent,
+    HomeComponent,
+    ServiceComponent,
+    TeamComponent,
+    ContactComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/footers/footers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footers',
+  imports: [],
+  templateUrl: './footers.component.html',
+  styleUrl: './footers.component.scss'
+})
+export class FootersComponent {
+
+}
